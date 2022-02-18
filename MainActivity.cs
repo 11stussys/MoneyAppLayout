9357dc999d7d59b87d4db3a0a9d5f5ac95759727
11stussyS <sz.stus@gmail.com>
2022-02-18 21:33:04 +0100

    final layout

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,7 +1,9 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
+using Calligraphy;
 
 namespace MoneyApp
 {
@@ -11,8 +13,18 @@ namespace MoneyApp
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
+
+            CalligraphyConfig.InitDefault(new CalligraphyConfig.Builder()
+                .SetDefaultFontPath("fonts/GlacialIndifference-Regular.ttf")
+                .SetFontAttrId(Resource.Attribute.fontPath)
+                .Build());
+
             SetContentView(Resource.Layout.activity_main);
+        }
+
+        protected override void AttachBaseContext(Context @base)
+        {
+            base.AttachBaseContext(CalligraphyContextWrapper.Wrap(@base));
         }
     }
 }
